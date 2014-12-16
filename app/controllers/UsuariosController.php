174336f47c5ed436c6543abbe3a222986ea8b224
Julio Bonis <drbonis@gmail.com>
2014-12-16 23:16:52 +0100

    cambios

--- app/controllers/UsuariosController.php
+++ app/controllers/UsuariosController.php
@@ -3,7 +3,14 @@ class UsuariosController extends BaseController {
   public function mostrarUsuarios()
   {
     $usuarios = Usuario::all();
-    return View::make('usuarios/lista', array('usuarios'=> $usuarios));
+    if (Auth::validate($credentials))
+      {
+          return View::make('usuarios/lista', array('usuarios'=> $usuarios));
+      }
+      else {
+        return View::make('usuarios/lista', array('usuarios'=> array()));
+      }
+
   }
 }
 ?>
--- app/views/usuarios/lista.blade.php
+++ app/views/usuarios/lista.blade.php
@@ -1,11 +1,19 @@
-<h1>
-  Usuarios
-</h1>
 
-<ul>
-  @foreach($usuarios as $usuario)
-    <li>
-      {{$usuario->nombre.' '.$usuario->apellido}}
-    </li>
-  @endforeach
-</ul>  
+
+
+@extends('layouts.master')
+
+@section('container')
+    <h1>
+      Usuarios
+    </h1>
+
+    <ul>
+      @foreach($usuarios as $usuario)
+        <li>
+          {{$usuario->nombre.' '.$usuario->apellido}}
+        </li>
+      @endforeach
+    </ul>
+    <p>{{}}</p>
+@stop
